240b1cd89c300631b92e441bcf70260690249070
Pinaki Bhattacharjee <48859773+pinakipb2@users.noreply.github.com>
2020-05-31 21:37:39 +0530

    Managed Gibberish

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #Made with https://www.weatherbit.io/
-from flask import Flask, render_template, flash, make_response, url_for, request, redirect
+from flask import Flask, render_template, flash, make_response, url_for, request, redirect,flash
 import requests
 
 app = Flask(__name__)
+app.secret_key = 'bgc/di.[o;,lyog-/[min06edbh/['
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -10,10 +11,13 @@ def index():
     place = None
     if request.method == "POST":
         place = request.form['place']
-        r = requests.get('https://api.weatherbit.io/v2.0/current?city='+place+'&key=your_api_key')
-        json_object = r.json()
-        for item in json_object['data']:
-            temp = float(item['app_temp'])
+        r = requests.get('https://api.weatherbit.io/v2.0/current?city='+place+'&key=your_api_key_here')
+        try:
+            json_object = r.json()
+            for item in json_object['data']:
+                temp = float(item['app_temp'])
+        except:
+            flash('Please Enter a Correct Place!','warning')
     return render_template('index.html', temp=temp,place=place)
 
 
